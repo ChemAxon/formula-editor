78c5ac9eb2966b86d47684d9dcfc001e3d29eb2d
BoriEri <ebiro@chemaxon.com>
2019-02-26 17:07:42 +0100

    Emoji set.

--- src/assets/symbols.js
+++ src/assets/symbols.js
@@ -288,9 +288,148 @@ const symbols = [
             {
                 name: 'double dagger',
                 code: '&#8225;'
+            }
+        ]
+    },
+    {
+        title: 'Emojis',
+        characters: [
+            {
+                name: 'atom',
+                code: '&#9883;'
             },
-
-
+            {
+                name: 'aesculapius',
+                code: '&#9877;'
+            },
+            {
+                name: 'microscope',
+                code: '&#128300;'
+            },
+            {
+                name: 'telescope',
+                code: '&#128301;'
+            },
+            {
+                name: 'alembic',
+                code: '&#9879;'
+            },
+            {
+                name: 'petri dish',
+                code: '&#129515;'
+            },
+            {
+                name: 'test tube',
+                code: '&#129514;'
+            },
+            {
+                name: 'DNA',
+                code: '&#129516;'
+            },
+            {
+                name: 'microbe',
+                code: '&#129440;'
+            },
+            {
+                name: 'labcoat',
+                code: '&#129404;'
+            },
+            {
+                name: 'goggles',
+                code: '&#129405;'
+            },
+            {
+                name: 'glass of milk',
+                code: '&#129371;'
+            },
+            {
+                name: 'fire extinguisher',
+                code: '&#129519;'
+            },
+            {
+                name: 'recycle',
+                code: '&#9851;'
+            },
+            {
+                name: 'skull and crossbons',
+                code: '&#9760;'
+            },
+            {
+                name: 'radioactive',
+                code: '&#9762;'
+            },
+            {
+                name: 'biohazard',
+                code: '&#9763;'
+            },
+            {
+                name: 'warning',
+                code: '&#9888;'
+            },
+            {
+                name: 'snowflake',
+                code: '&#10052;'
+            },
+            {
+                name: 'fire',
+                code: '&#128293;'
+            },
+            {
+                name: 'collision',
+                code: '&#128165;'
+            },   
+            {
+                name: 'scientis woman',
+                code: '&#128105;'
+            },  
+            {
+                name: 'genie man',
+                code: '&#129502;'
+            },
+            {
+                name: 'scientist man',
+                code: '&#128104;'
+            },  
+            {
+                name: 'smiling face',
+                code: '&#128578;'
+            },   
+            {
+                name: 'winking face',
+                code: '&#128521;'
+            },  
+            {
+                name: 'thinking face',
+                code: '	&#129300;'
+            },  
+            {
+                name: 'face with rolling eyes',
+                code: '&#128580;'
+            },  
+            {
+                name: 'face with medical mask',
+                code: '&#128567;'
+            },  
+            {
+                name: 'thumbs up',
+                code: '&#128077;'
+            },  
+            {
+                name: 'thumbs down',
+                code: '&#128078;'
+            },
+            {
+                name: 'thumbs down',
+                code: '&#128078;'
+            },
+            {
+                name: 'thumbs down',
+                code: '&#128078;'
+            },
+            {
+                name: 'poo',
+                code: '&#128169;'
+            }
         ]
     }
 ];
